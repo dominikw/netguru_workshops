5917faa6dee532db8d6648d8fe75da928eb02abb
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-05 19:36:16 +0100

    Unify buttons, layout improvement

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	var formHeight = $(".hiddenForm").height();
+	var h = 0;
+	$(".hiddenForm").css({height: '0px'});
+
+	$(".newHidden").click(function(){
+		if (h == 0){
+			h = formHeight
+			$("span.arrow").html("&#9650;");
+		}else{
+			h = 0;
+			$("span.arrow").html("&#9660;");
+		}
+		$(".hiddenForm").animate({
+			height: h
+		}, 600)
+	});
+
+
+});
