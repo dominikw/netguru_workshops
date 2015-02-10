465b49bc81c39391f0f0a7f204a9490aaa9927d7
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-10 18:26:48 +0100

    Enable user to edit firstname and lastname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
 	  def configure_permitted_parameters
 	    devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
+	    devise_parameter_sanitizer.for(:account_update) << :firstname << :lastname
 	  end
 
 end
