7754a4494089df308d295547209cbb149e5caf05
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-10 17:19:07 +0100

    Shorten review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates :content, presence: true
-  validates :rating, presence: true
-  validates :user_id, presence: true
+  validates :content, :rating, :user_id, presence: true
 end
